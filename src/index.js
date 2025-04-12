aead0d854ef978c1c45a7b25f11f0207358c179c
tmuziringa <tmuziringa@gmail.com>
2025-04-12 03:52:55 +0100

    updated js with api intergration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-function generateVerse(event){
-    event.preventDefault();
-
+function displayVerse(response){
+    console.log("verse generated")
 new Typewriter("#verse", {
-  strings: "John 14vs1 :Let not your heart be troubled",
+  strings: response.data.answer,
   autoStart: true,
   delay: 1,
   cursor: "",
 });
+
+}
+
+
+function generateVerse(event){
+    event.preventDefault();
+
+
+    let instructionInput=document.querySelector("#user-instruction");
+    let apiKey="0bb03dodf4t67bd073b315986695ca76";
+    let prompt=`generate a bible verse about ${instructionInput.value}`;
+    let context="You are a well read AI assistant who knows all the verses in the holy bible.Your mission is to provide 3 verses in basic HTML from the bible that relate to the user instruction.do not include the word html"
+    let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey} `
+console.log("generating verse");
+console.log(`Prompt:${prompt}`);
+console.log(`Context:${context}`)
+
+    axios.get(apiUrl).then(displayVerse)
+
 
 }
 
