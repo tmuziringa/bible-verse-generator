65b52a8e505b2181e89de43077b588e9d37d239a
tmuziringa <tmuziringa@gmail.com>
2025-04-12 05:47:05 +0100

    improved the user experience

--- src/index.js
+++ src/index.js
@@ -19,7 +19,11 @@ function generateVerse(event){
     let prompt=`generate a bible verse about ${instructionInput.value}`;
     let context="You are a well read AI assistant who knows all the verses in the holy bible.Your mission is to provide 3 verses in basic HTML from the bible that relate to the user instruction.do not include the word html"
     let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey} `
-console.log("generating verse");
+
+let verseElement=document.querySelector("#verse");
+verseElement.classList.remove("hidden");
+verseElement.innerHTML=`<div class="blink">Retrieving verses relating to ${instructionInput.value}</div>`;
+    console.log("generating verse");
 console.log(`Prompt:${prompt}`);
 console.log(`Context:${context}`)
 
